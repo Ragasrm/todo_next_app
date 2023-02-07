befbf9e2293a035f7ee797204b0db469d11239e8
Raghav Endiran <raghav.endiran@SYSVINEMP139.local>
2023-02-07 18:47:13 +0530

    Feat: Header section was made with styles

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,12 +1,13 @@
 'use client'
-import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material'
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
+import styles from './header.module.css'
 import React from 'react'
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <AppBar position="static">
+    <AppBar position="static" className={styles.appBarContainer}>
     <Toolbar>
       {/* <IconButton
         size="large"
@@ -18,9 +19,9 @@ export default function Header({}: Props) {
         <MenuIcon />
       </IconButton> */}
       <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        News
+        To Do App
       </Typography>
-      <Button color="inherit">Login</Button>
+      {/* <Button color="inherit">Login</Button> */}
     </Toolbar>
   </AppBar>
   )
